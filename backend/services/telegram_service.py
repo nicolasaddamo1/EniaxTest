d50d9e090f4b7bf9d48cc7e1b959ba41dc743113
Nicolas <nicolasaddamo1@gmail.com>
2025-04-18 18:16:19 -0300

    feat: Added telegramservice fil

---
+++ backend/services/telegram_service.py
@@ -0,0 +1,8 @@
+import requests
+from config import TELEGRAM_BOT_TOKEN
+
+def send_message(chat_id, text):
+    url = f"https://api.telegram.org/bot{TELEGRAM_BOT_TOKEN}/sendMessage"
+    payload = {"chat_id": chat_id, "text": text}
+    response = requests.post(url, json=payload)
+    return response.json()
