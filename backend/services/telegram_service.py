adf1f6653cf518b3ebad0c178f117173d14de757
Nicolas <nicolasaddamo1@gmail.com>
2025-04-18 19:24:05 -0300

    feat: changes on service

--- backend/services/telegram_service.py
+++ backend/services/telegram_service.py
@@ -1,8 +1,15 @@
 import requests
 from config import TELEGRAM_BOT_TOKEN
 
-def send_message(chat_id, text):
-    url = f"https://api.telegram.org/bot{TELEGRAM_BOT_TOKEN}/sendMessage"
+BASE_URL = f"https://api.telegram.org/bot{TELEGRAM_BOT_TOKEN}"
+
+def send_message(chat_id: str, text: str):
+    url = f"{BASE_URL}/sendMessage"
     payload = {"chat_id": chat_id, "text": text}
-    response = requests.post(url, json=payload)
-    return response.json()
+    res = requests.post(url, json=payload)
+    return res.json()
+
+def get_bot_info():
+    url = f"{BASE_URL}/getMe"
+    res = requests.get(url)
+    return res.json()
